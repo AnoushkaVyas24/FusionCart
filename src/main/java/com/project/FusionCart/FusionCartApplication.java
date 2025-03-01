a97f3d52525fbff7e2241f66758d28d00b9bce08
Vinsee <anuvyas2405@gmail.com>
2025-03-02 02:51:45 +0530

    Project initialized

---
+++ src/main/java/com/project/FusionCart/FusionCartApplication.java
@@ -0,0 +1,13 @@
+package com.project.FusionCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusionCartApplication.class, args);
+	}
+
+}
